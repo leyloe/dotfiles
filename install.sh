f6a40f9f621f46095a45f557e1c1b394729a3ea8
Your Name <you@example.com>
2025-05-09 22:29:51 -0500

    asdf

--- install.sh
+++ install.sh
@@ -1,1 +1,15 @@
 #!/usr/bin/env bash
+
+sudo rm /etc/nixos/configuration.nix
+
+rm -r ~/.config/waybar
+rm -r ~/.config/helix
+rm -r ~/.config/wofi
+rm -r ~/.config/sway
+rm -r ~/.config/foot
+
+ln -s ~/.dotfiles/home/.config/waybar ~/.config/waybar
+ln -s ~/.dotfiles/home/.config/helix ~/.config/helix
+ln -s ~/.dotfiles/home/.config/wofi ~/.config/wofi
+ln -s ~/.dotfiles/home/.config/sway ~/.config/sway
+ln -s ~/.dotfiles/home/.config/foot ~/.config/foot
