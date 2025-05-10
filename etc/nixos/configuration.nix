86ed6dffd9c10dcdbc02aec14ce6e18570fd5188
Your Name <you@example.com>
2025-05-09 22:13:02 -0500

    .

---
+++ etc/nixos/configuration.nix
@@ -0,0 +1,102 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+  
+  # Boot loader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  
+  # Networking
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+
+  # Time and Locale
+  time.timeZone = "America/Chicago";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+  
+  # Users
+  users.users.user = {
+    isNormalUser = true;
+    description = "user";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+  
+  # Additional packages
+  environment.systemPackages = with pkgs; [
+    # wm
+    swaylock
+    swayidle
+    swaynotificationcenter
+    swaysettings
+    networkmanagerapplet
+    waybar
+    wofi
+    grim
+    jq
+    wl-clipboard
+
+    # utils
+    firefox
+    git
+    unzip
+    helix
+
+    # looks
+    adwaita-icon-theme
+  ];
+
+  fonts.packages = with pkgs; [
+    nerdfonts
+  ];
+
+  # Gui
+  services.gnome.gnome-keyring.enable = true;
+
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+  };
+  
+  # User apps
+  services.flatpak.enable = true;
+
+  # File managment
+  programs.thunar.enable = true;
+  programs.xfconf.enable = true;
+  services.gvfs.enable = true;
+  services.tumbler.enable = true;
+
+  programs.thunar.plugins = with pkgs.xfce; [
+    thunar-archive-plugin
+    thunar-volman
+  ];
+
+  # Firewall
+  networking.firewall.enable = true;
+
+  # Version
+  system.stateVersion = "24.11";
+}
