99884f9c511e707792aaced69f6b30df6b580bfe
Your Name <you@example.com>
2025-05-12 21:02:43 -0500

    user

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 HOME_CONFIG_DIR="$HOME/.config"
 ETC_NIX_CONFIG="/etc/nixos/configuration.nix"
 
-LOCAL_CONFIG_DIR="$SCRIPT_DIR/.config"
+LOCAL_CONFIG_DIR="$SCRIPT_DIR/home/.config"
 LOCAL_NIX_CONFIG="$SCRIPT_DIR$ETC_NIX_CONFIG"
 
 sudo chmod a-w "$LOCAL_NIX_CONFIG"
