1aa751d01f46d9873f8c3a8158f07df2f58ffaae
Your Name <you@example.com>
2025-05-12 20:17:48 -0500

    update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-LOCAL_CONFIG_DIR="$SCRIPT_DIR/home/.config"
-HOME_CONFIG_DIR="$HOME/.config"
 CONFIGS=(waybar helix wofi sway foot)
-LOCAL_NIX_CONFIG="$SCRIPT_DIR/etc/nixos/configuration.nix"
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+HOME_CONFIG_DIR="$HOME/.config"
 ETC_NIX_CONFIG="/etc/nixos/configuration.nix"
 
-chmod a-w "$LOCAL_NIX_CONFIG"
+LOCAL_CONFIG_DIR="$SCRIPT_DIR$HOME_CONFIG_DIR"
+LOCAL_NIX_CONFIG="$SCRIPT_DIR$ETC_NIX_CONFIG"
+
+sudo chmod a-w "$LOCAL_NIX_CONFIG"
 sudo rm "$ETC_NIX_CONFIG"
 echo "removed ${ETC_NIX_CONFIG}"
 
